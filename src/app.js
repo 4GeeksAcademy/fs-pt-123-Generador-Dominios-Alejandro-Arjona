26839dba367b36a77e0e2a4362c7f853812fa0de
Alejandro Arjona López <alex.arjona@hotmail.com>
2025-10-15 23:12:28 +0000

    Creado y finalizado html, js , css

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  let listaDominios = "";
+
+  pronoun.map(p => {
+    adj.map(a => {
+      noun.map(n => {
+        listaDominios += `<hr><p>${p}${a}${n}.com<hr></p>`;
+      });
+    });
+  });
+
+  const lista = document.querySelector('#dominios');
+  lista.innerHTML = listaDominios;
 };
